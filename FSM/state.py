e05589e601be10c46c7944069d726dccc0c3d9ae
ZoRex15 <shpakstas007@gmail.com>
2023-10-30 22:02:18 +0300

    Added new states

--- FSM/state.py
+++ FSM/state.py
@@ -11,4 +11,26 @@ class FSMTakeTheTest(StatesGroup):
     question_5 = State()
     end_poll = State()
 
+class FSMAdminState(StatesGroup):
+    in_admin_menu = State()
+
+class FSMUpdateQuestion(StatesGroup):
+    request_test = State()
+    request_question = State()
+    request_new_question = State()
+
+class FSMUpdateOption(StatesGroup):
+    request_test = State()
+    request_question = State()
+    request_new_option = State()
+
+class FSMUpdateAnswerText(StatesGroup):
+    request_test = State()
+    request_question = State()
+    request_possible_answer = State()
+    request_answer_text = State()
+
+
+
+
 
