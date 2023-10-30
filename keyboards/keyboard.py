ad7650e5a1e6e65d5951bbcb44fe3478a9822fa3
ZoRex15 <shpakstas007@gmail.com>
2023-10-30 21:30:11 +0300

    Added functions for creating a keyboard with choosing a test and question.

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -8,9 +8,28 @@ start_setting: ReplyKeyboardMarkup = ReplyKeyboardMarkup(one_time_keyboard=True,
 keyboard_menu = ReplyKeyboardMarkup(one_time_keyboard=True, keyboard=[
     [KeyboardButton(text='Меню')]
 ])
+
+keyboard_request_new_option = ReplyKeyboardMarkup(one_time_keyboard=True, keyboard=[
+    [KeyboardButton(text='a)'), KeyboardButton(text='b)'), KeyboardButton(text='c)')]
+])
+
 def create_ticket_keyboard():
-    bilder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+    bilder = ReplyKeyboardBuilder()
     for index in range(1, 26):
         bilder.add(KeyboardButton(text=f'Билет {index}'))
     bilder.adjust(3)
+    return bilder.as_markup(resize_keyboard=True)
+
+def create_tests_keyboard():
+    bilder = ReplyKeyboardBuilder()
+    for index in range(1, 26):
+        bilder.add(KeyboardButton(text=f'Тест {index}'))
+    bilder.adjust(3)
+    return bilder.as_markup(resize_keyboard=True)
+
+def create_question_keyboard():
+    bilder = ReplyKeyboardBuilder()
+    for index in range(1, 6):
+        bilder.add(KeyboardButton(text=f'Вопрос {index}'))
+    bilder.adjust(1)
     return bilder.as_markup(resize_keyboard=True)
