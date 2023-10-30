f616272e8f0bc44da095e9b9ac71ff45ae57a672
ZoRex15 <shpakstas007@gmail.com>
2023-10-30 22:08:06 +0300

    A new parameter has been added admin_ids

--- config/config.py
+++ config/config.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class TgBot:
     token: str
+    admin_ids: list[int]
 
 @dataclass
 class Config:
@@ -12,4 +13,8 @@ class Config:
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+    return Config(tg_bot=TgBot(
+                token=env('BOT_TOKEN'),
+                admin_ids=list(map(int, env.list('ADMIN_IDS')))
+                ),
+                )
