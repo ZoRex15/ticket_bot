45cca0d9b3129de72e5270cfd8960f12c4410e1c
ZoRex15 <shpakstas007@gmail.com>
2023-11-06 14:55:26 +0300

    Added a function for creating a pagination

--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -1,4 +1,6 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 
 languages = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='🇷🇺Русский🇷🇺', callback_data='ru')],
@@ -13,6 +15,7 @@ help_menu = InlineKeyboardMarkup(inline_keyboard=[
 menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='📜Перейти к билетам📜', callback_data='go_to_ticket')],
     [InlineKeyboardButton(text='📄Перейти к тестам📄', callback_data='go_to_tests')],
+    [InlineKeyboardButton(text='📊Результаты тестов📊', callback_data='test_results_list')],
     [InlineKeyboardButton(text='🔧Настройки⚙️', callback_data='settings')]
 ])
 
@@ -21,3 +24,16 @@ admin_menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Изменить вариант ответа', callback_data='update_option')],
     [InlineKeyboardButton(text='Изменить текст варианта ответа', callback_data='update_answer_text')]
 ])
+
+def create_pagination_inline_keyboard(page):
+    bilder = InlineKeyboardBuilder()
+    buttons = {
+        '<<': 'back',
+        f'{page}/5': 'empoty',
+        '>>': 'forward'
+    }
+    for text, callback_data in buttons.items():
+        bilder.add(InlineKeyboardButton(text=text, callback_data=callback_data))
+    bilder.adjust(3)
+    return bilder.as_markup()
+
