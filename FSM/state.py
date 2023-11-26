06b50e553aae7464e903016f8a90c2b325354768
ZoRex15 <shpakstas007@gmail.com>
2023-11-26 20:14:01 +0300

    Removing unnecessary conditions. Adding a ticket of ticket selection

--- FSM/state.py
+++ FSM/state.py
@@ -1,7 +1,11 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class FSMSettings(StatesGroup):
     language_selection = State()
+
+class FSMTakeTheTicket(StatesGroup):
+    ticket_choice = State()
 
 class FSMTakeTheTest(StatesGroup):
     question_1 = State()
@@ -13,24 +17,3 @@ class FSMTakeTheTest(StatesGroup):
 
 class FSMAdminState(StatesGroup):
     in_admin_menu = State()
-
-class FSMUpdateQuestion(StatesGroup):
-    request_test = State()
-    request_question = State()
-    request_new_question = State()
-
-class FSMUpdateOption(StatesGroup):
-    request_test = State()
-    request_question = State()
-    request_new_option = State()
-
-class FSMUpdateAnswerText(StatesGroup):
-    request_test = State()
-    request_question = State()
-    request_possible_answer = State()
-    request_answer_text = State()
-
-
-
-
-
