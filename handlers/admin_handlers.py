ff4af08a70405af7853080068f99af1e89030ae1
ZoRex15 <shpakstas007@gmail.com>
2023-12-10 22:22:59 +0300

    append handlers to work with the newsletter

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -4,12 +4,13 @@ from aiogram.filters import StateFilter, Command
 from aiogram.fsm.state import default_state
 from aiogram.fsm.context import FSMContext
 
+
 from lexicon.lexicon import LEXICON
-from FSM.state import FSMAdminState
-from service.service import Database
+from FSM.state import FSMAdminState, FSMSpam
+from service.service import Database, RabbitMQ
 from config.config import Config, load_config
 from filters.filters import IsAdmin
-from keyboards.inline_keyboard import admin_menu
+from keyboards.inline_keyboard import admin_menu, confirmation_of_the_newsletter
 
 
 router = Router()
@@ -22,8 +23,47 @@ async def send_admin_menu(message: Message, state: FSMContext):
     await message.answer(LEXICON['admin_menu'], reply_markup=admin_menu)
     await state.set_state(FSMAdminState.in_admin_menu)
 
-@router.callback_query(F.data == 'the_number_of_users')
+@router.callback_query(F.data == 'the_number_of_users', IsAdmin(config.tg_bot.admin_ids))
 async def the_number_of_users(callback: CallbackQuery, state: FSMContext):
     the_number_of_users = Database.get_the_number_of_users()
     await callback.message.answer(text=f'Количество пользователей: {the_number_of_users}')
+    await state.clear()
+
+@router.callback_query(F.data == 'start_spam', IsAdmin(config.tg_bot.admin_ids))
+async def start_spam(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(LEXICON['start_spam'])
+    await state.set_state(FSMSpam.the_text_of_the_blower)
+
+@router.message(StateFilter(FSMSpam.the_text_of_the_blower), IsAdmin(config.tg_bot.admin_ids))
+async def the_text_of_the_blower(message: Message, state: FSMContext):
+    Database.set_chat_id(admin_id=message.from_user.id,
+                         chat_id=message.chat.id,)
+    Database.set_message_id(admin_id=message.from_user.id,
+                            message_id=message.message_id)
+    await message.answer(
+        text='Вы точно хотите это разослать?',
+        reply_markup=confirmation_of_the_newsletter
+    )
+    await state.set_state(FSMSpam.confirmation_of_the_newsletter)
+
+@router.callback_query(F.data == 'yes', StateFilter(FSMSpam.confirmation_of_the_newsletter), IsAdmin(config.tg_bot.admin_ids))
+async def сonfirmation_of_the_newsletter(callback: CallbackQuery, state: FSMContext):
+    chat_id = Database.get_chat_id(callback.from_user.id)
+    message_id = Database.get_message_id(callback.from_user.id)
+    await callback.message.answer(
+        text='Запускаю рассылку!'
+    )
+    await RabbitMQ.send_message_spam_queue(
+        chat_id=chat_id, 
+        message_id=message_id
+    )
+    Database.clear_spam_settings(callback.from_user.id)
+    await state.clear()
+
+@router.callback_query(F.data == 'no', StateFilter(FSMSpam.confirmation_of_the_newsletter), IsAdmin(config.tg_bot.admin_ids))
+async def сonfirmation_of_the_newsletter(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(
+        text='Отменяю'
+    )
+    Database.clear_spam_settings(callback.from_user.id)
     await state.clear()
