6b6a63c49a90960d945613eb33b0bd5bcba4df78
ZoRex15 <shpakstas007@gmail.com>
2023-12-25 13:36:59 +0300

    add new path

--- path/path.py
+++ path/path.py
@@ -3,3 +3,7 @@
 path_ticket_ru: dict = {f'билет {index}': f'ticket_ru/Билет {index}.pdf' for index in range(1, 26)}
 
 path_ticket_by: dict = {f'билет {index}': f'ticket_by/Б{index}.pdf' for index in range(1, 26)}
+
+path_ticket_ru_txt: dict = {f'билет {index}': f'ticket_ru/Билет {index}.txt' for index in range(1, 26)}
+
+path_ticket_by_txt: dict = {f'билет {index}': f'ticket_by/Б{index}.txt' for index in range(1, 26)}
